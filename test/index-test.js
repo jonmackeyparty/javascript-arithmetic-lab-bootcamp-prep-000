079480c805534b7ee16f6b99614228f313ed8297
Jon Mackey <jon@jonmackey.party>
2018-07-22 14:49:49 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -29,14 +29,13 @@ it('divide(a, b) divides a by b and returns the result', function() {
   expect(divide(a, b)).toEqual(a / b)
 })
 function inc(n){
-  return (n++)
+  return (n + 1)
 }
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
 })
 function dec(n){
-  var n = 147
-  return (n--)
+  return (n - 1)
 }
 it('dec(n) decrements n and returns the result', function() {
   expect(dec(a)).toEqual(a - 1)
